d83b013d0224f4e6aa3939c1b62a843621a3cf55
Fimmel <admin@fimmel.dev>
2025-04-02 09:33:43 +0200

    final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://F1mmel.github.io',
+  base: '/astroDemo',
 	integrations: [
 		starlight({
 			title: 'My Docs',
